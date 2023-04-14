5d55c1ec7bc88c7e338f883a311af1c98ae93fcc
Suraj Mhatre <surajmcodes@gmail.com>
2023-04-14 23:55:30 +0530

    initial commit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,11 +5,7 @@ const Footer = () => {
     return (
         <footer>
             <p>
-                Created with <i className="fa fa-heart" style={{color: "#FF5252"}}></i> by
-                <a href="https://www.linkedin.com/in/mansimar-anand/" target="_blank" rel="noopener noreferrer" className="link">
-                    <span> </span>
-                    Mansimar Anand
-                </a>
+               
             </p>
         </footer>
     );
